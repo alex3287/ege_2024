a92dffa1fe863a0d6107ce04880ebd514aa9d58d
Alexander Mangazeev <alex3287@bk.ru>
2023-09-05 18:01:43 +0300

    first lesson

---
+++ progs/first_lesson/inf_2937.py
@@ -0,0 +1,11 @@
+# 12
+# The next number for the number 12 is 13.
+# The previous number for the number 12 is 11.
+
+number = int(input())
+
+next = number + 1
+previous = number - 1
+
+print('The next number for the number ', number, ' is ', next, '.', sep='')
+print(f'The previous number for the number {number} is {previous}.')
---
+++ progs/first_lesson/task_1.py
@@ -0,0 +1,17 @@
+# простой калькулятор
+# ввод данных
+x = int(input())
+y = int(input())
+
+# расчеты
+suma = x + y
+sub = x - y
+mult = x * y
+dev = x / y
+
+# вывод данных
+print(type(suma), suma)
+print(type(sub), sub)
+print(type(mult), mult)
+print(type(dev), dev)
+
