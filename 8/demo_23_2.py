77e65d0392a2dd87b301649426459147ae665b2c
alex3287 <alex3287@bk.ru>
2023-12-28 13:22:59 +0300

    KIM-8 demo_23

--- 8/demo_23_2.py
+++ 8/demo_23_2.py
@@ -8,7 +8,8 @@ for a in ABC:
                     word = a+b+c+d+e
                     # if a != '0' and len(set(word)) == 5 and int(a) % 2 != int(b) % 2 and int(b) % 2 != int(c) % 2 \
                     #         and int(c) % 2 != int(d) % 2 and int(d) % 2 != int(e) % 2:
-                    if a != '0' and word.count('6') == 1 and (a == '6' and int(b) % 2 != 0):
+                    if a != '0'
+                        '6' and int(b) % 2 != 0):
                         print(word)
                         count += 1
 print(count)
