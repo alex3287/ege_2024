0305a968f202086c28db5d7658ce96e5cae3633c
Преподаватель информатики кабинет №310 <kab310teacher@gym11.local>
2024-03-06 15:16:02 +0700

    kim-19-21

---
+++ 19-21/demo_24_kurs.py
@@ -0,0 +1,37 @@
+# +1 *2  129   1<=s<=128
+# задача 19
+# def F(s, pos):
+#     if s >= 129 and pos == 3: return True
+#     if s < 129 and pos == 3: return False
+#     if s >= 129: return False
+#
+#     if pos % 2 == 1:
+#         return F(s+1, pos+1) and F(s*2, pos+1)
+#     return F(s+1, pos+1) or F(s*2, pos+1)
+
+
+# задача 20
+# def F(s, pos):
+#     if s >= 129 and pos == 4: return True
+#     if s < 129 and pos == 4: return False
+#     if s >= 129: return False
+#
+#     if pos % 2 == 0:
+#         return F(s+1, pos+1) and F(s*2, pos+1)
+#     return F(s+1, pos+1) or F(s*2, pos+1)
+
+
+# задача 21
+def F(s, pos):
+    if s >= 129 and (pos == 3 or pos == 5): return True
+    if s < 129 and pos == 5: return False
+    if s >= 129: return False
+
+    if pos % 2 == 1:
+        return F(s+1, pos+1) and F(s*2, pos+1)
+    return F(s+1, pos+1) or F(s*2, pos+1)
+
+
+for s in range(1, 129):
+    if F(s, 1):
+        print(s)
