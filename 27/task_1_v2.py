167f184bb3d236e188b101446c5165ec89571386
Преподаватель информатики кабинет №310 <kab310teacher@gym11.local>
2024-04-26 10:37:19 +0700

    kim 27 2 ball

--- 27/task_1_v2.py
+++ 27/task_1_v2.py
@@ -1,21 +1,37 @@
-with open('task_1_A.txt') as fin:
+with open('task_1_B.txt') as fin:
     N = int(fin.readline())
-    A = [tuple(map(int, i.split())) for i in fin]
-
-Sums = []
-suma = 0
-for i in A:
-    suma += max(i)
-
-Sums.append(suma)
-for i in range(N):
     suma = 0
-    for j in range(N):
-        if i == j:
-            suma += min(A[j])
-        else:
-            suma += max(A[j])
-    Sums.append(suma)
+    sub_min = 10**10
+    for i in range(N):
+        a, b = map(int, fin.readline().split())
+        sub = abs(a-b)
+        if sub % 3:
+            sub_min = min(sub_min, sub)
+        suma += max(a, b)
+print(suma)
+if suma % 3 == 0:
+    suma -= sub_min
+print(suma)
 
-print(Sums)
-print(max(i for i in Sums if i % 3 != 0))
+# РЕШЕНИЕ на 1 БАЛЛ
+# with open('task_1_A.txt') as fin:
+#     N = int(fin.readline())
+#     A = [tuple(map(int, i.split())) for i in fin]
+#
+# Sums = []
+# suma = 0
+# for i in A:
+#     suma += max(i)
+#
+# Sums.append(suma)
+# for i in range(N):
+#     suma = 0
+#     for j in range(N):
+#         if i == j:
+#             suma += min(A[j])
+#         else:
+#             suma += max(A[j])
+#     Sums.append(suma)
+#
+# print(Sums)
+# print(max(i for i in Sums if i % 3 != 0))
--- 27/task_2_v2.py
+++ 27/task_2_v2.py
@@ -1,19 +1,45 @@
-with open('task_2_A.txt') as fin:
+with open('task_2_B.txt') as fin:
     N = int(fin.readline())
-    A = [int(i) for i in fin]
+    A_sum = [10**10] * 43
+    A_len = [0] * 43
+    suma = sum_max = 0
+    len_min = 10**10
+    for i in range(N):
+        x = int(fin.readline())
+        suma += x
+        d = suma % 43
+        if d == 0:
+            sum_max = suma
+            len_min = i+1
+        else:
+            sum_temp = suma - A_sum[d]
+            len_temp = i+1 - A_len[d]
+            if sum_temp > sum_max or (sum_temp == sum_max and len_temp < len_min):
+                sum_max = sum_temp
+                len_min = len_temp
+        if suma < A_sum[d]:
+            A_sum[d] = suma
+            A_len[d] = i+1
+print(len_min)
 
-sum_max = 0
-min_len = 10**10
 
-for i in range(N):
-    suma = 0
-    cur_len = 0
-    for j in range(i, N):
-        suma += A[j]
-        cur_len += 1
-        if suma % 43 == 0:
-            if suma > sum_max or (suma==sum_max and cur_len<min_len):
-                sum_max = suma
-                min_len = cur_len
-
-print(min_len)
+# РЕШЕНИЕ на 1 БАЛЛ
+# with open('task_2_A.txt') as fin:
+#     N = int(fin.readline())
+#     A = [int(i) for i in fin]
+#
+# sum_max = 0
+# min_len = 10**10
+#
+# for i in range(N):
+#     suma = 0
+#     cur_len = 0
+#     for j in range(i, N):
+#         suma += A[j]
+#         cur_len += 1
+#         if suma % 43 == 0:
+#             if suma > sum_max or (suma==sum_max and cur_len<min_len):
+#                 sum_max = suma
+#                 min_len = cur_len
+#
+# print(min_len)
